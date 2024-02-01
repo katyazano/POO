a459efb219684e108df68c37e3751a6f3f7f4d05
Katheryn Azano <katheryn.azano@gmail.com>
2024-02-01 10:39:08 -0600

    En este commit está el reto 2

--- src/edu/katheryn_azano/reto2/process/procesos.java
+++ src/edu/katheryn_azano/reto2/process/procesos.java
@@ -1,6 +1,21 @@
 package edu.katheryn_azano.reto2.process;
 
-import edu.katheryn_azano.reto2.ui.CLI;
-public class procesos {
+import java.util.Scanner;
 
+public class procesos {
+    public static void login(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingresa el usuario");
+        String usuario = scanner.next();
+        System.out.println("Ingresa la contraseña");
+        String password = scanner.next();
+        String userCorrecto = "usuario1";
+        String passwordCorrecto = "password123";
+
+        if(!(usuario.equals(userCorrecto) && password.equals(passwordCorrecto))) {
+            System.out.println("Usuario o contraseña incorrectos, intente de nuevo");
+            login();
+        }
+    }
 }
--- src/edu/katheryn_azano/reto2/ui/CLI.java
+++ src/edu/katheryn_azano/reto2/ui/CLI.java
@@ -1,72 +1,85 @@
 package edu.katheryn_azano.reto2.ui;
 
+import edu.katheryn_azano.reto2.process.procesos;
+
+import java.util.Scanner;
+
 public class CLI {
-    public static void login(){
+    public static int showMenu(){
         Scanner scanner = new Scanner(System.in);
-
-            System.out.println("Ingresa el usuario");
-            String usuario = scanner.next();
-            System.out.println("Ingresa la contraseña");
-            String password = scanner.next();
-
-            if (usuario == "usuario1", password == "password123"){
-                launchApp();
-            } else{
-                System.out.println("Usuario o contraseña incorrectos, intente de nuevo");
-                login();
-            }
-
-    }
-
-    /**
-     * Este metodo despliega el menu de opciones al usuario
-     */
-    public static void showMenu(){
         System.out.println("Elige una de las siguientes opciones: \n1. Comprar cajas de papel \n2. Vender cajas de papel \n3. Mostrar reporte \n4. Salir del programa");
+        System.out.println("Ingrese la opción deseada: ");
+        int choice = scanner.nextInt();
+        return choice;
     }
 
     /**
      * Este metodo inicia la aplicación
      */
     public static void launchApp(){
+        //procesos.login();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese la opción deseada: ");
-        int choice = scanner.nextInt();
-        showMenu();
+        int choice = showMenu();
 
+        int total_cajas_vendidas = 0;
+        int total_cajas_compras = 0;
+        double monto_compras = 0;
+        double monto_ventas = 0;
+
+        int total_operaciones = 0;
         switch (choice) {
             case 1:
                 System.out.println("Ingrese el número de cajas a comprar: ");
-                int numeroCajas = scanner.nextInt();
-                System.out.println("Ingrese el costo de las cajas a vender: ");
-                double costoCajas = scanner.nextDouble();
-                double monto = costoCajas*numeroCajas;
-                System.out.println("La compra realizada por "+numeroCajas" cajas al costo de "+costoCajas" es igual a"+monto);
+                int numeroCajas1 = scanner.nextInt();
+                total_cajas_compras += numeroCajas1;
+                System.out.println("Ingrese el costo de las cajas a comprar: ");
+                double costoCajas1 = scanner.nextDouble();
+                double monto1 = costoCajas1 * numeroCajas1;
+                monto_compras += monto1;
+                System.out.println("La compra realizada por " + numeroCajas1 + " cajas al costo de " + costoCajas1 + " es igual a " + monto1);
+                total_operaciones += 1;
+                showMenu();
                 break;
 
             case 2:
-                System.out.println("Ingrese el número de cajas a comprar: ");
-                int numeroCajas = scanner.nextInt();
+                System.out.println("Ingrese el número de cajas a vender: ");
+                int numeroCajas2 = scanner.nextInt();
+                total_cajas_vendidas += numeroCajas2;
                 System.out.println("Ingrese el costo de las cajas a vender: ");
-                double costoCajas = scanner.nextDouble();
-                double monto = costoCajas*numeroCajas;
-                System.out.println("La venta realizada por "+numeroCajas" cajas al precio de "+costoCajas" es igual a"+monto);
+                double costoCajas2 = scanner.nextDouble();
+                double monto2 = costoCajas2 * numeroCajas2;
+                monto_ventas += monto2;
+                System.out.println("La venta realizada por " + numeroCajas2 + " cajas al precio de " + costoCajas2 + " es igual a " + monto2);
+                total_operaciones += 1;
+                showMenu();
                 break;
 
             case 3:
-                System.out.println("Ingrese la cantidad total de ventas: ");
-                int totalVentas = scanner.nextInt();
-                System.out.println("Ingrese la cantidad total de compras: ");
-                double cantidadCompras = scanner.nextDouble();
-                System.out.println("Ingrese los ingresos generados por ventas: ");
-                int ingresos = scanner.nextInt();
-                System.out.println("Ingrese los egresos generados por compras: ");
-                double egresos = scanner.nextDouble();
+                double monto_caja = monto_ventas-monto_compras;
+                System.out.println("La cantidad total de ventas: " + total_cajas_vendidas);
+                System.out.println("La cantidad total de compras: " + total_cajas_compras);
+                System.out.println("Los ingresos generados por ventas: " + monto_ventas);
+                System.out.println("Los egresos generados por compras: " + monto_compras);
+                System.out.println("Monto en la caja: " + monto_caja);
+                total_operaciones += 1;
+                showMenu();
                 break;
 
             case 4:
-                System.exit();
-                break;
+                monto_caja = monto_ventas-monto_compras;
+                System.out.println("La cantidad total de operaciones realizadas: " + total_operaciones);
+                System.out.println("La cantidad total de ventas: " + total_cajas_vendidas);
+                System.out.println("La cantidad total de compras: " + total_cajas_compras);
+                System.out.println("Los ingresos generados por ventas: " + monto_ventas);
+                System.out.println("Los egresos generados por compras: " + monto_compras);
+                System.out.println("Monto en la caja: " + monto_caja);
+                System.out.println("Hasta luego tonoto(s)");
+                return;
+
+            default:
+                System.out.println("Opción invalida. Seleccione una correcta");
+                showMenu();
         }
+
     }
 }
